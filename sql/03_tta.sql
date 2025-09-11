ac8f5858ae4f17439c7d1884095ce830a4e5dd41
Asuka Osuki <asukaape.4th@gmail.com>
2025-09-11 11:38:10 -0400

    Update 03_tta.sql

--- sql/03_tta.sql
+++ sql/03_tta.sql
@@ -15,26 +15,8 @@ SELECT
 FROM tta
 WHERE tta.first_answer IS NOT NULL
       AND TIMESTAMP_DIFF(tta.first_answer,tta.question_time, HOUR) >= 0   -- Remove NULL and invalid data
-ORDER BY time_duration 
-LIMIT 20;
+ORDER BY hour_to_answer
 
-
-WITH tta AS(
-  SELECT 
-   q.id, 
-   q.creation_date AS question_time,
-   MIN(a.creation_date) AS first_answer
-FROM `bigquery-public-data.stackoverflow.posts_questions` AS q
-JOIN bigquery-public-data.stackoverflow.posts_answers AS a
-ON q.id = a.parent_id
-GROUP BY 1,2
-)
-SELECT 
-  tta.id,
-  TIMESTAMP_DIFF(tta.first_answer,tta.question_time, HOUR) AS hour_to_answer
-FROM tta
-WHERE tta.first_answer IS NOT NULL
-      AND TIMESTAMP_DIFF(tta.first_answer,tta.question_time, HOUR) >= 0
 
 -- Calculate TTA by tag
 WITH base AS(
